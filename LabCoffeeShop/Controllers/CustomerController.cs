b254a89b34c3a387daf573dba3e214ad3bf9a2e1
Adam Horn <Adam@Adam-Horns-MacBook-Pro.local>
2018-05-22 10:08:46 -0400

    first commit

---
+++ LabCoffeeShop/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LabCoffeeShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult Register()
+        {
+            return View();
+        }
+		public IActionResult RegisterSuccess(string firstName, string lastName, string email, string number, string password)
+        {
+            ViewBag.firstName = firstName;
+            ViewBag.lastName = lastName;
+            ViewBag.email = email;
+			ViewBag.number = number;
+			ViewBag.password = password;
+            return View();
+        }
+		public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabCoffeeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabCoffeeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
